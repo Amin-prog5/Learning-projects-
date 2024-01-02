80c8f8eb3dabc1cf06923f8b7164d5f5e666d455
Amin <155407117+Amin-prog5@users.noreply.github.com>
2024-01-02 07:18:59 +0200

    Code _1
    Code that calculates the tax after adding a value.

---
+++ first_pro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+cout << "program calculate the tax\n ";   
+int price;
+cin >> price ;
+int new_price = price * 0.14 + price ;
+cout << new_price;
+
+return 0;
+
+    
+}
