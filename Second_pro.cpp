c4767f87da888288bf8d85ec2f2accdca680afcd
Amin <155407117+Amin-prog5@users.noreply.github.com>
2024-01-02 22:20:47 +0200

    Second_pro.cpp
     code that displays odd or even numbers

---
+++ Second_pro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int num;
+cout << "enter any number\n";
+cin >> num;
+if (num % 2 == 0)
+{
+cout << "number is even";
+}
+else if (num != 0)
+{
+cout << "number is odd";
+}
+    
+    
+    return 0;
+}
