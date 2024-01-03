118ea0d755dfd086156c6e36dd7e4282db5883d8
Amin <155407117+Amin-prog5@users.noreply.github.com>
2024-01-03 05:24:20 +0200

    fourth_pro.cpp
    A program that calculates the sum of numbers until they add a negative number

---
+++ fourth_pro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num=0 , sum=0;
+while (num >=0)
+{
+cout <<"enter num\n";
+cin >> num;
+sum+= num ;
+}
+cout << sum; "\n";
+    
+
+    return 0; 
+    
+}
