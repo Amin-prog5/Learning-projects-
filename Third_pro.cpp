2d07aa4451eac83c1dd5d4bc6c3c4e9183cbb93d
Amin <155407117+Amin-prog5@users.noreply.github.com>
2024-01-03 03:27:45 +0200

    Third_pro.cpp
    Code that compares two numbers.

---
+++ Third_pro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num1;
+int num2;
+cout << " enter num 1\n";
+cin >> num1;
+cout << "enter number 2\n";
+cin >> num2;
+if (num1 > num2)
+{
+cout << num1;
+}
+if (num2 > num1)
+{
+    cout <<  num2;
+}
+else
+cout << "Both are equal";
+
+    return 0; 
+    
+}
